8a8b8bd61730c6a8ce41153474b4d4b4b0c3b175
Rich <rdhouck@gmail.com>
2018-06-07 17:36:02 -0700

    udpated stuff

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+# Create A Blog models
+# Title
+# Pub_date
+# Body
+#Image
+
+# Add the Blog app to the settings
+
+
+# Create a migration
+
+
+# Migrate
+
+
+# Add to admin
+
+
+class Blog(models.Model):
+    title = models.Charfield(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/') 
