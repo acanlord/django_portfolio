61bc466ea5b7b90b9c0b7e0facbdd304fad89fce
Rich <rdhouck@gmail.com>
2018-06-20 16:13:58 -0700

    added new files

--- blog/models.py
+++ blog/models.py
@@ -1,26 +1,22 @@
 from django.db import models
 
 
-# Create A Blog models
-# Title
-# Pub_date
-# Body
-#Image
-
-# Add the Blog app to the settings
-
-
-# Create a migration
-
-
-# Migrate
-
-
-# Add to admin
-
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
-    image = models.ImageField(upload_to='images/') 
+    image = models.ImageField(upload_to='images/')
+
+
+
+    def __str__(self):
+        return self.title
+
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
