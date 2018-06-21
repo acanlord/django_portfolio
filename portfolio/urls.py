d24dfb8c76e3fce1c010ea88387422e529e542b7
Rich <rdhouck@gmail.com>
2018-06-21 11:42:42 -0700

    added new files

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,6 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
 
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
